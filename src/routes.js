cea362c5b6cf3ed5388e8ea7ac47d1f089157efc
kallelrm <kallelr@outlook.com>
2020-02-27 23:11:36 -0300

    Creating Recipient controller and routes

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
 
 routes.get('/users', UserController.store);
 
+routes.post('/recipients', RecipientController.store);
 routes.post('/sessions', SessionController.store);
 
 export default routes;
